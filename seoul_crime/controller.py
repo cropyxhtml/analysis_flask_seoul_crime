7d110477df5fa23a7e19145d3349831750e94bcf
cropyx <cropyx@gmail.com>
2019-08-31 09:50:12 +0900

    morning start

--- seoul_crime/controller.py
+++ seoul_crime/controller.py
@@ -3,12 +3,14 @@ from seoul_crime.cctv_pop import CCTVModel
 from seoul_crime.crime_police import CrimeModel
 from seoul_crime.foliumtest import FoliumTest
 from seoul_crime.police_nml import PoliceNmlModel
+from seoul_crime.crime_map import CrimeMap
 class Controller:
     def __init__(self):
         self._cctv = CCTVModel()
         self._crime =CrimeModel()
         self._usa =FoliumTest()
         self._police = PoliceNmlModel()
+        self._crime_map = CrimeMap()
 
     def execute(self):
         # cctv = self._cctv
@@ -17,8 +19,11 @@ class Controller:
         # crime = self._crime
         # crime.hook_process()
 
-        usa = self._usa
-        usa.hook()
+        # usa = self._usa
+        # usa.hook()
 
         # pn= self._police
         # pn.hook()
+
+        crime_map = self._crime_map
+        crime_map.hook()
